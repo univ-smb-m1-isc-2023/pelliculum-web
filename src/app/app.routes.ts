e0de3ab7a49370b9bc6d24057e8067c164a21e57
Mathis Mazoyer <mathis.mazoyer@etu.univ-smb.fr>
2024-02-09 11:44:32 +0100

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import {Routes} from '@angular/router';
+import {ProfileComponent} from "./modules/profile/profile.component";
+import {HomeComponent} from "./modules/home/home.component";
+
+export const routes: Routes = [
+  { path: "", component: HomeComponent},
+  { path: "profile", component: ProfileComponent},
+];
