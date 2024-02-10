711b32eaaf41a770726012d564bbc1a146c16349
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-02-10 18:35:39 +0100

    v1.0001

--- src/app/modules/header/header-sign/header-sign.component.ts
+++ src/app/modules/header/header-sign/header-sign.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import {NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header-sign',
   standalone: true,
   imports: [
-    NgIf
+    NgIf,
+    RouterLink
   ],
   templateUrl: './header-sign.component.html',
   styleUrls: ['./header-sign.component.sass']
 })
 export class HeaderSignComponent {
   connected = true;
+  notif = 3;
 
 }
