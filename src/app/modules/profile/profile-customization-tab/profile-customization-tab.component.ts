a0472f14f321f5d47a6c7f19fe9919e90b97c824
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-02-10 16:47:10 +0100

    v1

--- src/app/modules/profile/profile-customization-tab/profile-customization-tab.component.ts
+++ src/app/modules/profile/profile-customization-tab/profile-customization-tab.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {InputCustomComponent} from "../../../shared/components/input-custom/input-custom.component";
 
 @Component({
   selector: 'app-profile-customization-tab',
   standalone: true,
-  imports: [],
+  imports: [
+    InputCustomComponent
+  ],
   templateUrl: './profile-customization-tab.component.html'
 })
 export class ProfileCustomizationTabComponent {
@@ -12,6 +15,8 @@ export class ProfileCustomizationTabComponent {
   name = 'Paul'
   lastname = 'Smith'
   email = 'paul.smith@gmail.com'
+  photo = 'https://www.w3schools.com/howto/img_avatar.png'
+  pseudo = 'paulsmith'
 
 
 }
---
+++ src/app/shared/components/input-custom/input-custom.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-input-custom',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-custom.component.html',
+  styles : []
+})
+export class InputCustomComponent {
+  constructor() {}
+
+  @Input() value: string | undefined;
+
+  @Input() size : string | undefined;
+}
