bf0416f19c5f9dc971cad7993ba2cbe56e0ccc39
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-02-13 10:09:23 +0100

    setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router';
 import {ProfileComponent} from "./modules/profile/profile.component";
 import {HomeComponent} from "./modules/home/home.component";
+import {GameClassicComponent} from "./modules/games/game-classic/game-classic.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent},
   { path: "profile", component: ProfileComponent},
+  { path: "game/classic", component : GameClassicComponent}
 ];
