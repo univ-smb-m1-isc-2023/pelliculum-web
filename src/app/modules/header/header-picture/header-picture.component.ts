4f3726c75b425bf07cb1e9c8d3db526dbd962fb5
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-02-13 09:54:09 +0100

    router link

--- src/app/modules/header/header-picture/header-picture.component.ts
+++ src/app/modules/header/header-picture/header-picture.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header-picture',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header-picture.component.html'
 })
 export class HeaderPictureComponent {
