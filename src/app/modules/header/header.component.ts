31a474cb6d4b3ebaf7056c9a47958fb8c4c9f07a
Mathis Mazoyer <realdragonma@gmail.com>
2024-02-18 19:23:50 +0100

    style: icon

--- src/app/modules/header/header.component.ts
+++ src/app/modules/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderSignComponent } from './components/header-sign/header-sign.component';
 import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-  imports: [HeaderSignComponent, RouterLink],
+  imports: [HeaderSignComponent, RouterLink, NgOptimizedImage],
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {}
