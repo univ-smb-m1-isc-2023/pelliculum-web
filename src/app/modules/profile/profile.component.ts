83b0cd069b640ddf4cce6c2c1c14048a20cd8e5a
Mathis Mazoyer <mathis.mazoyer@etu.univ-smb.fr>
2024-03-01 18:56:32 +0100

    profile rework search bar

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProfileTabsComponent } from './components/profile-tabs/profile-tabs.component';
 import { ProfileSecurityTabComponent } from './components/profile-security-tab/profile-security-tab.component';
 import { ProfileCustomizationTabComponent } from './components/profile-customization-tab/profile-customization-tab.component';
 import { NgIf } from '@angular/common';
+import { TmdbService } from '../../core/services/tmdb.service';
+import { BackdropComponent } from '../../shared/components/backdrop/backdrop.component';
+import { AxiosService } from '../../core/services/axios.service';
 
 @Component({
     selector: 'app-profile',
     standalone: true,
-    imports: [ProfileTabsComponent, ProfileSecurityTabComponent, ProfileCustomizationTabComponent, NgIf],
+    imports: [ProfileTabsComponent, ProfileSecurityTabComponent, ProfileCustomizationTabComponent, NgIf, BackdropComponent],
     templateUrl: './profile.component.html',
     styles: ``
 })
-export class ProfileComponent {
-    activeTab: string = 'customization';
+export class ProfileComponent implements OnInit {
 
-    constructor() {}
+    activeTab: string = 'customization';
+    movie: any
+
+    constructor(private tmdbService: TmdbService, protected axiosService: AxiosService) {}
+
+    ngOnInit() {
+        this.tmdbService.getTopMovies().subscribe((data: any) => {
+            this.movie = data.results[0];
+        });
+    }
 
     selectTab(tab: string) {
         this.activeTab = tab;
