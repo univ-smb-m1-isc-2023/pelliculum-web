b886782f78a27c45c82bfafbce72a21675d8ca38
Mathis Mazoyer <mathis.mazoyer@etu.univ-smb.fr>
2024-03-07 15:56:53 +0100

    cloudflare

--- src/app/modules/header/components/header-search/header-search.component.ts
+++ src/app/modules/header/components/header-search/header-search.component.ts
@@ -74,5 +74,5 @@ export class HeaderSearchComponent implements OnInit, OnDestroy {
     getPosterUrl(posterPath : string): string {
         return `https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}`;
     }
-    
+
 }
