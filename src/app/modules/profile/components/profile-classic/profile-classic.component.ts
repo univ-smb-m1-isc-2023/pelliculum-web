d0a32fcebda7eddebc588cef0d5f68b5514d08bb
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-03-07 18:59:24 +0100

    v1.1

--- src/app/modules/profile/components/profile-classic/profile-classic.component.ts
+++ src/app/modules/profile/components/profile-classic/profile-classic.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { PosterComponent } from '../../../../shared/components/poster/poster.component';
+import { StarsComponent } from '../../../../shared/components/stars/stars.component';
 
 @Component({
   selector: 'app-profile-classic',
   standalone: true,
   imports: [
     PosterComponent,
+    StarsComponent,
   ],
   templateUrl: './profile-classic.component.html'
 })
