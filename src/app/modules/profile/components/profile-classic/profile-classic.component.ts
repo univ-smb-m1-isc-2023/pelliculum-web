526a88517320f674e9549b0d90343401e5e1c7fd
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-03-07 16:14:38 +0100

    v1

--- src/app/modules/profile/components/profile-classic/profile-classic.component.ts
+++ src/app/modules/profile/components/profile-classic/profile-classic.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { PosterComponent } from '../../../../shared/components/poster/poster.component';
 
 @Component({
   selector: 'app-profile-classic',
   standalone: true,
-  imports: [],
+  imports: [
+    PosterComponent,
+  ],
   templateUrl: './profile-classic.component.html'
 })
 export class ProfileClassicComponent {
