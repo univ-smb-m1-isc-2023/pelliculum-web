980fa7c7568f2a9445f193dc5a976337bcf45996
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-03-09 18:50:44 +0100

    cooking

--- src/app/modules/profile/components/profile-friends/profile-friends.component.ts
+++ src/app/modules/profile/components/profile-friends/profile-friends.component.ts
@@ -20,34 +20,48 @@ export class ProfileFriendsComponent implements OnInit {
 
   protected friendName: string = '';
   protected contacts: any[] = [];
+  protected shownContacts: any[] = [];
   protected followers: any[] = [];
   protected follows: any[] = [];
+  protected searchQuery: string = '';
 
   constructor(
     private userService: UserService,
   ) {
   }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.userService.getFollows().then(r => {
-      this.follows = r
+      this.follows = r;
       this.contacts = this.follows;
+      this.shownContacts = this.contacts;
     });
     this.userService.getFollowers().then(r => this.followers = r);
   }
 
-  switchFollowers(): void {
+  public switchFollowers(): void {
     this.contacts = this.followers;
+    this.shownContacts = this.contacts;
   }
-  switchFollows(): void {
+
+  public switchFollows(): void {
     this.contacts = this.follows;
+    this.shownContacts = this.contacts;
   }
+
+  public search(): void {
+    this.shownContacts = this.contacts.filter(contact => contact.username.includes(this.searchQuery));
+  }
+
+
 
   protected addFriend(): void {
     this.userService.addFriend(this.friendName).then(
-      r => this.notyf.success(r.response.data)
+      r => {
+        this.notyf.success(r);
+      },
     ).catch(
-      r => this.notyf.error(r.response.data)
+      r => this.notyf.error(r.response.data),
     );
   }
 
