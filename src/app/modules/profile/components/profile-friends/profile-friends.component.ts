028ac6cfffb47f5c3a581306d6593382ca062a45
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-03-10 10:12:57 +0100

    cooking this shit

--- src/app/modules/profile/components/profile-friends/profile-friends.component.ts
+++ src/app/modules/profile/components/profile-friends/profile-friends.component.ts
@@ -4,6 +4,7 @@ import { UserService } from '../../../../core/services/user.service';
 import { FormsModule } from '@angular/forms';
 import { Notyf } from 'notyf';
 import { log } from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-profile-friends',
@@ -11,6 +12,7 @@ import { log } from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
   imports: [
     TablerIconsModule,
     FormsModule,
+    NgClass,
   ],
   templateUrl: './profile-friends.component.html',
 })
@@ -25,12 +27,21 @@ export class ProfileFriendsComponent implements OnInit {
   protected follows: any[] = [];
   protected searchQuery: string = '';
 
+  public activeTab: string = 'follows';
+  marker: HTMLDivElement | undefined;
+
+
   constructor(
     private userService: UserService,
   ) {
   }
 
   public ngOnInit(): void {
+    this.getNetwork();
+    this.initMarker();
+  }
+
+  public getNetwork(): void {
     this.userService.getFollows().then(r => {
       this.follows = r;
       this.contacts = this.follows;
@@ -39,20 +50,38 @@ export class ProfileFriendsComponent implements OnInit {
     this.userService.getFollowers().then(r => this.followers = r);
   }
 
+  public selectTab(tabName: string) {
+    this.activeTab = tabName;
+  }
+
+  public initMarker(): void {
+    this.marker = document.getElementById('markerNetwork') as HTMLDivElement;
+    const privateTab: HTMLDialogElement | null = document.getElementById('follows') as HTMLDialogElement;
+    privateTab.click();
+  }
+
+  indicatorTab(e: any): void {
+    console.log(e);
+    if (!this.marker || !e) return;
+    this.marker.style.left = e.target.offsetLeft + 10 + 'px';
+    this.marker.style.width = e.target.offsetWidth -18+ 'px';
+  }
+
   public switchFollowers(): void {
     this.contacts = this.followers;
     this.shownContacts = this.contacts;
+    this.selectTab('followers')
   }
 
   public switchFollows(): void {
     this.contacts = this.follows;
     this.shownContacts = this.contacts;
+    this.selectTab('follows')
   }
 
   public search(): void {
     this.shownContacts = this.contacts.filter(contact => contact.username.includes(this.searchQuery));
   }
-
 
 
   protected addFriend(): void {
