8def6483ad4fec3529a4b2017afcbdbd39d55cba
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-03-19 12:52:48 +0100

    cooking

--- src/app/modules/movie-details/components/movie-details-rating/movie-details-rating.component.ts
+++ src/app/modules/movie-details/components/movie-details-rating/movie-details-rating.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { StarsComponent } from '../../../../shared/components/stars/stars.component';
+import { StarsHoverableComponent } from '../../../../shared/components/stars-hoverable/stars-hoverable.component';
+import { UserService } from '../../../../core/services/user.service';
 
 @Component({
     selector: 'app-movie-details-rating',
     standalone: true,
-    imports: [StarsComponent],
+    imports: [StarsComponent, StarsHoverableComponent],
     templateUrl: './movie-details-rating.component.html',
     styleUrls: ['./movie-details-rating.sass']
 })
-export class MovieDetailsRatingComponent {
+export class MovieDetailsRatingComponent implements OnInit{
     @Input() id: number = 0;
     @Input() ratings: any[] = this.generateRandomRatings(5);
+
+    profilePicture: string = 'https://www.w3schools.com/howto/img_avatar.png';
+
+    constructor(protected user: UserService) {}
+
+    ngOnInit(): void {
+        if (!this.user.isLoggedIn()) return;
+        this.profilePicture = `http://localhost:8080/profilePictures/${this.user.getUsername()}.jpeg`;
+    }
 
     generateRandomRatings(count: number): any[] {
         const randomRatings = [];
