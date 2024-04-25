f29e1fa418ae79541bac3e5692c3addb06782178
Mathis Mazoyer <realdragonma@gmail.com>
2024-04-25 21:38:39 +0200

    h-full footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './modules/header/header.component';
 import { FooterComponent } from './modules/footer/footer.component';
 import { StarRatingComponent } from 'angular-star-rating';
+import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent, NgIf],
     templateUrl: './app.component.html',
     styles: ``
 })
