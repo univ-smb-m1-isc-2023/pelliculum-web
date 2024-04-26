aa9aac0c46ad1c5dd0333c54ca36e46fab297c1a
Mathis Mazoyer <mathis.mazoyer@etu.univ-smb.fr>
2024-04-26 12:51:52 +0200

    p

--- src/app/core/services/axios.service.ts
+++ src/app/core/services/axios.service.ts
@@ -8,6 +8,7 @@ import { Response } from '../../shared/models/response.model';
 export class AxiosService {
     constructor() {
         //axios.defaults.baseURL = 'http://localhost:8080';
+        //
         axios.defaults.baseURL = 'https://rapi.pelliculum.oups.net/';
     }
 
