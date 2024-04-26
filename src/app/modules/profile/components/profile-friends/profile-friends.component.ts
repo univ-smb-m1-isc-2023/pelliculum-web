33009b2883ad4cf81bab4015c11f8adb0ad57252
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-04-26 09:31:50 +0200

    fix follower

--- src/app/modules/profile/components/profile-friends/profile-friends.component.ts
+++ src/app/modules/profile/components/profile-friends/profile-friends.component.ts
@@ -38,8 +38,15 @@ export class ProfileFriendsComponent implements OnInit {
     public async getNetwork(): Promise<void> {
         const follows = (await this.userService.getFollowsDetails()).data;
         const followers = (await this.userService.getFollowersDetails()).data;
+        console.log('follows ',follows);
+        console.log('followers ', followers);
         for (const follow of follows) {
             follow.isFollowed = true;
+        }
+        for (const follower of followers) {
+            if (follows.some((f : any): boolean => f.username === follower.username)) {
+                follower.isFollowed = true;
+            }
         }
         this.follows = follows;
         this.followers = followers;
